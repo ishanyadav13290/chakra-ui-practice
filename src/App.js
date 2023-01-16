fbca0076afb0f247dc0bf57a414f69d5f915771d
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-01-16 15:45:02 +0530

    error screen added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Center, ChakraProvider } from '@chakra-ui/react';
+import { Box, Center, ChakraProvider, CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css';
@@ -9,23 +9,28 @@ import PagesButton from './components/Pages';
 
 function App() {
   let [data, setData] = useState([]);
-  let [loading, setLoading] = useState(false)
+  let [loading, setLoading] = useState(false);
+  let [error, setError] = useState(false);
   let [page, setPage] = useState(1);
   let url = `https://jsonplaceholder.typicode.com/photos?_limit=9&_page=${page}`;
   useEffect(() => {
     setLoading(true)
     axios
       .get(url)
-      .then((res) => {setData(res.data) 
-        setLoading(false)})
-      .catch((err) => console.log(err));
+      .then((res) => {setLoading(false)
+        setData(res.data) 
+        })
+      .catch((err) => {
+        setError(true)
+      setLoading(false)});
   }, [page]);
-  // console.log(page)
-  return loading?(<Center h="100%"><h1>Loading...</h1></Center>): (
+
+  return loading?<Box><Center><CircularProgress isIndeterminate color='green.300' /></Center> </Box>: error?<h1>An Error Occured! Please try again!</h1>:(
     <ChakraProvider>
       <div className="App">
       <Navbar />
       <Boxes data={data}/>
+      
     <PagesButton setPage={setPage} page={page} />
     </div>
     </ChakraProvider>
